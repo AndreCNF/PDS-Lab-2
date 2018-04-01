f1127baa80aa5d57dc8427ef5f2ed7aeff62daf2
AndreCNF <andrecnf@gmail.com>
2018-04-01 02:57:55 +0100

    Partially fixed dtmfdecode?
    It seems to be calculating the peaks correctly, except that it also adds peaks with 0 height? wtf

--- Lab2.m
+++ Lab2.m
@@ -6,18 +6,18 @@
 
 %% Teste para observar valores experimentais de frequencias de sinusoides
 
-phoneKeys = ['1', '5', '9', '#'];
-toneDuration = 40;
-pauseDuration = 40;
-amplitude = 1;
-noiseLevel = 0;
-samplingFrequency = 8000;
-
-phoneSignal = dtmfencode(phoneKeys, toneDuration, pauseDuration, amplitude, noiseLevel, samplingFrequency);
-[S,F,T] = spectrogram(phoneSignal);
-surf(T, F*1000, abs(S));
-xlabel('t(ms)');
-ylabel('f(Hz)');
+% phoneKeys = ['1', '5', '9', '#'];
+% toneDuration = 40;
+% pauseDuration = 40;
+% amplitude = 1;
+% noiseLevel = 0;
+% samplingFrequency = 8000;
+% 
+% phoneSignal = dtmfencode(phoneKeys, toneDuration, pauseDuration, amplitude, noiseLevel, samplingFrequency);
+% [S,F,T] = spectrogram(phoneSignal);
+% surf(T, F*1000, abs(S));
+% xlabel('t(ms)');
+% ylabel('f(Hz)');
 
 %%
 % From figure SinFreqTest:
@@ -30,3 +30,9 @@ ylabel('f(Hz)');
 % F21 = 951.1
 % F22 = 1049
 % F23 = 1160
+
+true_peaks = dtmfdecode(S, F, T);
+[row,col] = find(true_peaks);
+surf(T, F*1000, abs(S));
+hold on
+plot3(T(col),F(row)*1000,abs(S(row, col)),'r*','MarkerSize',24)
--- dtmfdecode.m
+++ dtmfdecode.m
@@ -1,42 +1,121 @@
 % Find the true 3D peaks / local maxima in a dtmf
 function true_peaks = dtmfdecode(S, F, T)
-    locs_f = zeros(2, length(T));
-    locs_t = zeros(2, length(F));
-    
-    % Maximum of 10 tones
-    true_peaks = zeros(3, 10);
+%     locs_f = zeros(2, length(T));
+%     locs_t = zeros(2, length(F));
+%     
+%     % Maximum of 10 tones
+%     true_peaks = zeros(3, 10);
+% 
+%     % Find local maxima in time domain
+%     for i = 1:length(T)
+%         % Find local maxima (peaks) sorted by tallest to shortest
+%         [PKS, LOCS] = findpeaks(abs(S(:,i)), 'SortStr', 'descend');
+%         
+%         if length(LOCS) < 2
+%             locs_f(:, i) = [-1; -1];
+%             continue;
+%         end
+%         
+%         % Only the 2 highest peaks matter (only 2 sinusoides per tone)
+%         locs_f(:, i) = LOCS(1:2, 1);
+%     end
+%     
+%     % Find local maxima in frequency domain
+%     for i = 1:length(F)
+%         % Find local maxima (peaks) sorted by tallest to shortest
+%         [PKS, LOCS] = findpeaks(abs(S(i,:))', 'SortStr', 'descend');
+%         
+%         if length(LOCS) < 2
+%             locs_t(:, i) = [-1; -1];
+%             continue;
+%         end
+%         
+%         % Only the 2 highest peaks matter (only 2 sinusoides per tone)
+%         locs_t(:, i) = LOCS(1:2, 1);
+%     end
+%     
+%     n = 1;
+%     
+%     % Match maxima from time and frequency domains to get the true peaks
+%     for i = 1:length(T)
+%         for j = 1:length(F)
+%             for k = 1:2
+%                 for l = 1:2
+%                     % if locs_f(k, i) == F(j) && locs_t(j, l) == T(i)
+%                     if locs_f(k, i) == j && locs_t(j, l) == i
+%                         true_peaks(:, n) = [S(F(j), T(i)), F(j), T(i)];
+%                         n = n +1;
+%                     end
+%                 end
+%             end
+%         end
+%     end
 
-    % Find local maxima in time domain
-    for i = 1:length(T)
-        % Find local maxima (peaks) sorted by tallest to shortest
-        [PKS, LOCS] = findpeaks(abs(S(:,i)), 'SortStr', 'descend');
-        
-        % Only the 2 highest peaks matter (only 2 sinusoides per tone)
-        locs_f(:, i) = LOCS(1:2, 1);
-    end
+
+%    [row,col] = find(imregionalmax(abs(S)));
+%    hold on
+%    plot3(T(col),F(row)*1000,abs(S(row, col)),'r*','MarkerSize',24)
+
+
+    true_peaks = zeros(length(F), length(T));
     
-    % Find local maxima in frequency domain
+    % Check for peaks that are reagional maxima
     for i = 1:length(F)
-        % Find local maxima (peaks) sorted by tallest to shortest
-        [PKS, LOCS] = findpeaks(abs(S(i,:)), 'SortStr', 'descend');
-        
-        % Only the 2 highest peaks matter (only 2 sinusoides per tone)
-        locs_t(:, i) = LOCS(1:2, 1);
-    end
-    
-    n = 1;
-    
-    % Match maxima from time and frequency domains to get the true peaks
-    for i = 1:length(T)
-        for j = 1:length(F)
-            for k = 1:2
-                for l = 1:2
-                    if locs_f(k, i) == F(j) && locs_t(l, j) == T(i)
-                        true_peaks(:, n) = [S(F(j), T(i)), F(j), T(i)];
-                        n = n +1;
-                    end
+        for j = 1:length(T)
+            % Height treshold
+            if abs(S(i, j)) < 80
+                continue;
+            end
+            
+            try
+                if abs(S(i-1, j-1)) >= abs(S(i, j))
+                    continue;
                 end
             end
+            
+            try
+                if abs(S(i-1, j)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i-1, j+1)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i+1, j-1)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i+1, j)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i+1, j+1)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i, j-1)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            try
+                if abs(S(i, j+1)) >= abs(S(i, j))
+                    continue;
+                end
+            end
+            
+            true_peaks(i, j) = 1;
         end
     end
 end
